02f2fc2262fd211f69d49709375c79a5c67c51e0
Andreas J Mueller <amueller6@cnm.edu>
2021-03-10 10:17:58 +0000

    upgrade AWS provider to 3.31.0

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "2.65.0"
+      version = "~> 3.0"
     }
   }
 }
@@ -11,7 +11,7 @@ provider "aws" {
   region  = "us-west-2"
   profile = "liveproject"
 }
- 
+
 resource "aws_instance" "helloworld" {
   ami           = "ami-09dd2e08d601bff67"
   instance_type = "t2.micro"
